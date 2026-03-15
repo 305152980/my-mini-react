8e1636bce6a3a21eed2a9c69e07432847172c0ed
lijie <305152980@qq.com>
2026-03-15 21:55:36 +0800

    dev

---
+++ packages/react-reconciler/src/ReactFiberReconciler.ts
@@ -0,0 +1,13 @@
+import { type ReactNodeList } from '@my-mini-react/shared/ReactTypes'
+import { type FiberRoot } from './ReactInternalTypes'
+
+export function updateContainer(
+  element: ReactNodeList,
+  container: FiberRoot
+): void {
+  // 1、获取 current。
+  const current = container.current
+  current.memoizedState = { element }
+  // 2、调度更新。TODO
+  // scheduleUpdateOnFiber()
+}
