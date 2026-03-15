7929b9a95160e197ca2230d090638645a6030826
lijie <305152980@qq.com>
2026-03-15 21:19:58 +0800

    dev

--- packages/react-dom/src/client/ReactDOMRoot.ts
+++ packages/react-dom/src/client/ReactDOMRoot.ts
@@ -1,4 +1,6 @@
 import { type ReactNodeList } from '@my-mini-react/shared/ReactTypes'
+import { createFiberRoot } from '@my-mini-react/react-reconciler/src/ReactFiberRoot'
+import { type Container } from '@my-mini-react/react-reconciler/src/ReactInternalTypes'
 
 type RootType = {
   render: (children: ReactNodeList) => void
@@ -9,7 +11,8 @@ const ReactDOMRoot: ReactDOMRootCtor =
   function (): void {} as unknown as ReactDOMRootCtor
 ReactDOMRoot.prototype.render = function (children: ReactNodeList): void {}
 
-export function createRoot(): RootType {
+export function createRoot(container: Container): RootType {
+  const root: RootType = createFiberRoot(container)
   return new ReactDOMRoot()
 }
 
