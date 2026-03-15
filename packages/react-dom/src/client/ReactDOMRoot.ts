a95256f3312e4cab6b1b123c47f46f1ba74c6b1d
lijie <305152980@qq.com>
2026-03-15 21:33:50 +0800

    dev

--- packages/react-dom/src/client/ReactDOMRoot.ts
+++ packages/react-dom/src/client/ReactDOMRoot.ts
@@ -1,19 +1,30 @@
 import { type ReactNodeList } from '@my-mini-react/shared/ReactTypes'
 import { createFiberRoot } from '@my-mini-react/react-reconciler/src/ReactFiberRoot'
-import { type Container } from '@my-mini-react/react-reconciler/src/ReactInternalTypes'
+import type {
+  Container,
+  FiberRoot,
+} from '@my-mini-react/react-reconciler/src/ReactInternalTypes'
+import { updateContainer } from '@my-mini-react/react-reconciler/src/ReactFiberReconciler'
 
 type RootType = {
+  _internalRoot: FiberRoot
   render: (children: ReactNodeList) => void
 }
 
-type ReactDOMRootCtor = new () => RootType
-const ReactDOMRoot: ReactDOMRootCtor =
-  function (): void {} as unknown as ReactDOMRootCtor
-ReactDOMRoot.prototype.render = function (children: ReactNodeList): void {}
+type ReactDOMRootCtor = new (_internalRoot: FiberRoot) => RootType
+const ReactDOMRoot: ReactDOMRootCtor = function (
+  this: RootType,
+  _internalRoot: FiberRoot
+): void {
+  this._internalRoot = _internalRoot
+} as unknown as ReactDOMRootCtor
+ReactDOMRoot.prototype.render = function (children: ReactNodeList): void {
+  updateContainer(children, this._internalRoot)
+}
 
 export function createRoot(container: Container): RootType {
-  const root: RootType = createFiberRoot(container)
-  return new ReactDOMRoot()
+  const root: FiberRoot = createFiberRoot(container)
+  return new ReactDOMRoot(root)
 }
 
 export default {
