13f606255441ea3a84870685e0a9f2f335c565cc
lijie <305152980@qq.com>
2026-03-15 20:44:40 +0800

    dev

--- packages/react-reconciler/vite.config.ts
+++ packages/react-reconciler/vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig(({ mode }) => ({
     },
     // Rollup底层配置 - 定制库模式打包规则
     rollupOptions: {
-      external: ['@my-mini-react/scheduler'],
+      external: [], // 空数组表示无外部依赖，强制将shared+scheduler源码合并打入react-reconciler产物
       output: { globals: {} }, // 无全局依赖，留空即可
     },
   },
